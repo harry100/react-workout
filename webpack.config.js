a636841381259725845d5c68943855032ac52717
Amanda Maharjan <maharjanamanda@gmail.com>
2019-04-30 09:35:44 +0545

    react config setup and testing

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+const path = require('path');
+
+const config = {
+  entry: ['babel-polyfill', './lib/components/Index.js'],
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader', }
+    ]
+  }
+};
+
+module.exports = config;
